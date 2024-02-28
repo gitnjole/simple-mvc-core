84e03f05c38ba52f832dcf34f6cd2e80525672bb
Antonio Cahun <cahunantonio1@gmail.com>
2024-02-28 19:38:00 +0100

    intial: initial commit

---
+++ Controller.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\core;
+use app\core\Application;
+use app\core\middlewares\BaseMiddleware;
+
+class Controller
+{
+    public string $layout = 'main';
+    public string $action = '';
+
+    /**
+     * @var \app\core\middlewares\BaseMiddleware[]
+     */
+    protected array $middlewares = [];
+
+    public function render($view, $params = [])
+    {
+        return Application::$app->router->renderView($view, $params);
+    }
+
+    public function setLayout($layout)
+    {
+        $this->layout = $layout;
+    }
+
+    public function registerMiddleware(BaseMiddleware $middleware)
+    {
+        $this->middlewares[] = $middleware;
+    }
+
+    public function getMiddleware(): array
+    {
+        return $this->middlewares;
+    }
+}
